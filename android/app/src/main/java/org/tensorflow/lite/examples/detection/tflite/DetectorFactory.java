c3df0881e79ac967a811f6fc0226b04bbb05026f
Gang Deng <13918383742@163.com>
2023-04-23 14:44:28 +0800

    add sample code for reading yaml file

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/tflite/DetectorFactory.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/tflite/DetectorFactory.java
@@ -1,8 +1,19 @@
 package org.tensorflow.lite.examples.detection.tflite;
 
 import android.content.res.AssetManager;
+import android.util.Log;
 
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 public class DetectorFactory {
     public static YoloV5Classifier getDetector(
@@ -15,6 +26,29 @@ public class DetectorFactory {
         int[] output_width = new int[]{0};
         int[][] masks = new int[][]{{0}};
         int[] anchors = new int[]{0};
+
+        try{
+            YamlReader reader = null;
+            InputStream labelsInput = assetManager.open("coco.yaml");
+            BufferedReader br = new BufferedReader(new InputStreamReader(labelsInput));
+            String line;
+
+            reader = new YamlReader(new InputStreamReader(labelsInput));
+            Object object = reader.read();
+            Map map = (Map)object;
+            //String ret =(String) map.get("path");
+            Log.e("aaaa","path" + (String) map.get("path"));
+            Map names = (Map) map.get("names");
+            int name_size = names.size();
+            Log.e("aaaa","name count = " + name_size);
+            Object keys = names.keySet();
+            for(int i= 0;i<name_size;i++){
+                Log.e("aaaa",i + " " + (String)names.get(Integer.toString(i)));
+            }
+            Log.e("aaaa","aaa");
+        } catch (FileNotFoundException | YamlException e) {
+            e.printStackTrace();
+        }
 
         if (modelFilename.equals("yolov5s.tflite")) {
             labelFilename = "file:///android_asset/coco.txt";
